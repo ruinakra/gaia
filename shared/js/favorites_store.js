8b1d9f3756a765cdcab4211e0402011bec982a24
Alexander Chumakov <Alexander.Chumakov@harman.com>
2015-11-06 14:24:29 +0300

    Updated preliminary API for the favorites store

--- shared/js/favorites_store.js
+++ shared/js/favorites_store.js
@@ -1,254 +1,158 @@
-
 'use strict';
-
-/* global Promise */
 
 (function(exports) {
 
-  var datastore;
-
-
-  // Datastore name declared on the manifest.webapp
-  var DATASTORE_NAME = '';//'bookmarks_store';
-
-  // Indicates the initialization state
-  var readyState;
-
-  // Event listeners
-  var listeners = Object.create(null);
+  var dataStoreName;
 
   function init(dbName) {
-
-      console.log(' init 0, db = ' + dbName);
-
-      if (dbName ==='') {
-          return;
-      }
-
-      DATASTORE_NAME = dbName;
-
-    return new Promise(function doInit(resolve, reject) {
-      if (readyState === 'initialized') {
-        resolve();
-        return;
-      }
-
-      console.log(' init 1');
-      if (readyState === 'initializing') {
-        document.addEventListener('ds-initialized', function oninitalized() {
-          document.removeEventListener('ds-initialized', oninitalized);
-          resolve();
-        });
-        return;
-      }
-
-      readyState = 'initializing';
-
-      if (!navigator.getDataStores) {
-        console.error('Bookmark store: DataStore API is not working');
-        reject({ name: 'NO_DATASTORE' });
-        readyState = 'failed';
-        return;
-      }
-
-      console.log(' init 2');
-///////////////////////////////////////////////////////////////////////
-      navigator.getDataStores(DATASTORE_NAME).then(function(ds) {
-        if (ds.length < 1) {
-          console.error('Bookmark store: Cannot get access to the ' + DATASTORE_NAME);
-          reject({ name: 'NO_ACCESS_TO_DATASTORE' });
-          readyState = 'failed';
-          return;
-        }
-         console.log(' init 3');
-        datastore = ds[0];
-        //datastore.addEventListener('change', onchangeHandler);
-
-        console.log(' init 4');
-
-       // document.dispatchEvent(new CustomEvent('ds-initialized'));
-        resolve();
-      }, reject);
-     console.log(' init 5');
-///////////////////////////////////////////////////////////////////////
-    });
-      console.log(' init 6');
+    console.log("FavoritesStore::init(" + dbName + ")");
+    dataStoreName = dbName;
+    return;
   }
 
-  function doGetAll(resolve, reject) {
-      console.log('   doGetAll  0 ***, datastore = ' + datastore)
-
-    var result = Object.create(null);
-    var cursor = datastore.sync(); // datastore undefined
-
-    function cursorResolve(task) {
-      switch (task.operation) {
-        case 'update':
-        case 'add':
-           // console.log('   doGetAll  Add')
-          result[task.data.id] = task.data;
-          break;
-
-        case 'remove':
-        //    console.log('   doGetAll  remove')
-          delete result[task.data.id];
-          break;
-
-        case 'clear':
-          //  console.log('   doGetAll  clear')
-          result = Object.create(null);
-          break;
-
-        case 'done':
-            console.log('   doGetAll  done')
-            console.log(result)
-
-
-
-          resolve(result);
-          return;
-      }
-
-      cursor.next().then(cursorResolve, reject);
-    }
-
-    cursor.next().then(cursorResolve, reject);
-
-      console.log(' ***** doGetAll  END ***')
+  function getAllItems() {
+    console.log("FavoritesStore::getAllItems(" + dataStoreName + ")");
+    return;
   }
 
-  function get(dbName, id) {
-      if (dbName === '')
-          return;
-
-    return new Promise(function doGet(resolve, reject) {
-      init(dbName).then(function onInitialized() {
-        // datastore.get(id).then(resolve, reject);
-          var obj =  datastore.get(id).then(resolve, reject);
-          console.log(obj);
-      }, reject);
-    });
+  function insertItem(newItem, index) {
+    console.log("FavoritesStore::insertItem(" + dataStoreName + "), at:" + index);
+    return;
   }
-// ==============================================================
-    function getAll(dbName) {
-        if (dbName === '')
-            return;
 
-        console.log('    inside getAll 0, datastore= ' + datastore)
+  function moveItem(currentIndex, newIndex) {
+    console.log("FavoritesStore::moveItem(" + dataStoreName +
+                "), from:" + currentIndex + ", to:" + newIndex);
+    return;
+  }
 
-        var promise = new Promise(
-          function (resolve, reject) {
-            init(dbName).then(doGetAll(resolve, reject), reject);
-        });
+  function updateItem(newItem, index) {
+    console.log("FavoritesStore::updateItem(" + dataStoreName + "), at:" + index);
+    return;
+  }
 
-        return promise;
-    }
-// --------------------------------------------------------------------------------- MY
-    function httpGet(url) {
+  function deleteItem(index) {
+    console.log("FavoritesStore::deleteItem(" + dataStoreName + "), at:" + index);
+    return;
+  }
 
-      return new Promise(
-          function(resolve, reject) {
+  function clearAllItems() {
+    console.log("FavoritesStore::clearAllItems(" + dataStoreName + ")");
+    return;
+  }
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
+  function addItemEventListener(callback) {
+    console.log("FavoritesStore::addItemEventListener(" + dataStoreName + ")");
+    return;
+  }
 
-        xhr.onload = function() {
-          if (this.status == 200) {
-            resolve(this.response);
-          } else {
-            var error = new Error(this.statusText);
-            error.code = this.status;
-            reject(error);
-          }
-        };
+  function removeItemEventListener(callback) {
+    console.log("FavoritesStore::removeItemEventListener(" + dataStoreName + ")");
+    return;
+  }
 
-        xhr.onerror = function() {
-          reject(new Error("Network Error"));
-        };
+  function initActions(actions) {
+    console.log("FavoritesStore::initActions(" + dataStoreName + ")");
+    return;
+  }
 
-        xhr.send();
-      });
+  function getAction(actionId) {
+    console.log("FavoritesStore::getAction(" + dataStoreName + "), ID:" + actionId);
+    return;
+  }
 
-    } // ------------------------------------ MY
-
-  function clear(dbName) {
-      if (dbName === '')
-          return;
-
-    return new Promise(function doClear(resolve, reject) {
-      init(dbName).then(function onInitialized() {
-        datastore.clear().then(resolve, reject);
-      }, reject);
-    });
+  function getAllActions() {
+    console.log("FavoritesStore::getActions(" + dataStoreName + ")");
+    return;
   }
 
   exports.FavoritesStore = {
-   /*
-    * This method returns a bookmark object
-    *
-    * @param{String} String param that represents an identifier
-    */
-    get: get,
+    // **************************************
+    // FavoritesStore API definition        *
+    // **************************************
+    /**
+     * Initializes the favorites store object with provided application ID.
+     *
+     * @param{String} The application ID for the current favorites store object.
+     */
+    init: init,
 
-   /*
-    * This method returns an object of bookmarks indexed by id
-    */
-    getAll: getAll,
+    /**
+     * Gets all the favorite items in the store.
+     *
+     * @param {Date|Numeric} start start hour.
+     * @param {Date|Numeric} end end hour.
+     */
+    getAllItems: getAllItems,
 
-//   /*
-//    * Returns the latest revision UUID
-//    */
-//    getRevisionId: getRevisionId,
+    /**
+     * Inserts a new favorite item into the store.
+     *
+     * @param{Object} The new favorite item to be inserted.
+     * @param{Numeric} The index at which the item will be inserted.
+     */
+    insertItem: insertItem,
 
-//    /*
-//     * Method registers the specified listener on the API
-//     *
-//     * @param{String} A string representing the event type to listen for
-//     *
-//     * @param{Function} The method that receives a notification when an event of
-//     *                  the specified type occurs
-//     *
-//     */
-//    addEventListener: addEventListener,
+    /**
+     * Moves an existing item to a new location.
+     *
+     * @param{Numeric} The current index of the item to be moved.
+     * @param{Numeric} The new location for the item.
+     */
+    moveItem: moveItem,
 
-//    /*
-//     * Method removes the specified listener on the API
-//     *
-//     * @param{String} A string representing the event type to listen for
-//     *
-//     * @param{Function} The method that received a notification when an event of
-//     *                  the specified type occurs
-//     *
-//     */
-//    removeEventListener: removeEventListener,
+    /**
+     * Replace the existing item in the store with the new item.
+     *
+     * @param{Object} The updated item to be used in the store.
+     * @param{Numeric} The index at which the new item will be placed.
+     */
+    updateItem: updateItem,
 
-//    /*
-//     * This method adds a bookmark in the datastore
-//     *
-//     * @param{Object} The bookmark's data
-//     */
-//    add: add,
+    /**
+     * Removes the existing item in the store at provided index.
+     *
+     * @param{Numeric} The index at which the item will be removed.
+     */
+    deleteItem: deleteItem,
 
-//    /*
-//     * This method updates a bookmark in the datastore
-//     *
-//     * @param{Object} The bookmark's data
-//     */
-//     put: put,
+    /**
+     * Removes all the favorites items in the store.
+     */
+    clearAll: clearAllItems,
 
-//    /*
-//     * This method removes a bookmark from the datastore
-//     *
-//     * @param{String} The bookmark's id
-//     */
-//     remove: remove,
+    /**
+     * Registers a new listener object to listen to updates in the store.
+     *
+     * @param{Function} callback The callback function to be invoked when the store changes
+     */
+    addItemEventListener: addItemEventListener,
 
-//    /*
-//     *
-//     * This method clears the entire datastore, removing all entries.
-//     *
-//     */
-     clear: clear
+    /**
+     * Removes the specified event listener.
+     *
+     * @param{Function} The listener callback to be removed.
+     */
+    removeItemEventListener: removeItemEventListener,
+
+    /**
+     * Initializes the array of actions for the application.
+     *
+     * @param{Array} The array of all the actions supported by the application.
+     */
+    initActions: initActions,
+
+    /**
+     * Gets an action for the specified action ID.
+     *
+     * @param{Numeric} The action ID to be retrieved.
+     */
+    getAction: getAction,
+
+    /**
+     * Gets all the actions for the current application.
+     */
+    getAllActions: getAllActions,
   };
 
 }(window));
